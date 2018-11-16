0d1bce5ae7c6975146713cfede94209559b7853c
Aliaksandr Haurylenka <aliaksandr.haurylenka@gmail.com>
2018-11-16 13:47:33 +0300

    delfile

--- backend/views/yii2-app/layouts/left.php
+++ backend/views/yii2-app/layouts/left.php
@@ -43,8 +43,6 @@
                         ['label' => 'Венчики', 'icon' => 'table', 'url' => ['/venchik'],],
                         ['label' => 'Галерея', 'icon' => 'table', 'url' => ['/gallery'],],
                         ['label' => 'Награды', 'icon' => 'table', 'url' => ['/reward'],],
-                        ['label' => 'Загрузить', 'icon' => 'table', 'url' => ['/upload'],],
-                        ['label' => 'Удалить', 'icon' => 'table', 'url' => ['/del-img'],],
                         ['label' => 'Сообщения', 'icon' => 'table', 'url' => ['/save-message'],],
 
                     ],

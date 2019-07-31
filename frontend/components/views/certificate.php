0f775661af3b1cc8f16412a242e51e2a4f2ecddc
AliaksandrHaurylenka <aliaksandr.haurylenka@gmail.com>
2019-07-31 10:42:20 +0300

    asd

--- frontend/components/views/certificate.php
+++ frontend/components/views/certificate.php
@@ -6,6 +6,6 @@ PrettyPhotoAppAsset::register($this);
 ?>
 
 <?= Html::tag('div', Html::a(Html::img('/images/certificate-ru.jpg', ['alt' => 'Сертификат рус.']), '/images/certificate-ru.jpg', ['data-gal'=>"prettyPhoto[certificate]"])); ?>
-<?= Html::tag('div', Html::a(Html::img('/images/politika.jpg', ['alt' => 'Политика в области качества']), '/images/politika.jpg', ['data-gal'=>"prettyPhoto[certificate]"])); ?>
+<?= Html::tag('div', Html::a(Html::img('/images/politika.jpg', ['alt' => 'Политика в области качества']), '/images/politika-2019.jpg', ['data-gal'=>"prettyPhoto[certificate]"])); ?>
 <?= Html::tag('div', Html::a(Html::img('/images/declaration.jpg', ['alt' => 'Таможенная декларация']), '/images/declaration.jpg', ['data-gal'=>"prettyPhoto[certificate]"])); ?>
 <?= Html::tag('div', Html::a(Html::img('/images/certificate-en.jpg', ['alt' => 'Сертификат англ.']), '/images/certificate-en.jpg', ['data-gal'=>"prettyPhoto[certificate]"])); ?>
